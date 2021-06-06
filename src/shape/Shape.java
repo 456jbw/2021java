1bbea13940ec7d8e1920ecc80bdd0428e3dfcb0f
Costwen <47916831+Costwen@users.noreply.github.com>
2021-06-07 00:39:01 +0800

    ✨ feat: 所有形状类的基类

---
+++ src/shape/Shape.java
@@ -0,0 +1,92 @@
+package shape;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Stroke;
+
+import state.State;
+
+/**
+ * 这个类是所有图形类的基类,如果需要绘制图像,需要继承这个基类。
+ * @author Costwen
+ */
+public abstract class Shape {
+    protected int x1, y1; // 
+    protected Color color; // color
+    protected Stroke stroke;
+    protected State state;
+    /**
+     * 
+     * @param color 用于设置图形的颜色
+     * @param stroke 用于设置图形线条的大小
+     */
+    public Shape(Color color,Stroke stroke){
+        this.color = color;
+        this.stroke = stroke;
+        this.state = new State();
+    }
+/**
+ * 这个方法用于实现鼠标点击屏幕时,相对应的图形的行为。
+ * 
+ * @param x 这次点击时鼠标x轴的位置
+ * @param y 这次点击时鼠标y轴的位置
+ */
+    public void clickStrategy(int x, int y){
+
+    };
+
+    public void moveStrategy(int x,int y){
+
+    };
+    
+    public void dragStrategy(int x, int y){
+
+    };
+
+    public void releaseStrategy(int x, int y){
+
+    };
+
+    public abstract String toString();
+    public abstract void draw(Graphics2D g);
+
+    public int getX1() {
+        return x1;
+    }
+
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Stroke getStroke() {
+        return stroke;
+    }
+
+    public void setStroke(Stroke stroke) {
+        this.stroke = stroke;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+    
+}
