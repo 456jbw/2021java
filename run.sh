017b2f8cecf984447f611ccf939f39b89883f1e7
wyc <wyc-edu-zgbs@users.noreply.github.com>
2021-06-10 16:59:48 +0800

    fix start script
        correct shebang path
        remove unused JavaFX import

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!bash
+#!/bin/bash
 mkdir bin
 javac -cp ./src/ -d ./bin src/main/DrawDemo.java -encoding UTF-8
 java -cp ./bin/ main.DrawDemo
--- src/views/ButtonView.java
+++ src/views/ButtonView.java
@@ -1,7 +1,6 @@
 package views;
 import javax.swing.*;
 
-import javafx.scene.control.Button;
 import listener.MyDrawListener;
 import java.awt.*;
 
