bf95df75d3e69ea8694f3cc07015df8cd51799b2
Costwen <47916831+Costwen@users.noreply.github.com>
2021-06-10 15:50:31 +0800

    ✨ feat:
    实现了画板布局

--- src/views/Drawboard.java
+++ src/views/Drawboard.java
@@ -1,20 +1,41 @@
 package views;
 import javax.swing.JPanel;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 import java.util.List;
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import shape.Shape;
 import listener.*;
 
 public class Drawboard extends JPanel{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
     private BufferedImage bfimg;
     private Graphics bfpen;
-    
-    public Drawboard(MyDrawListener listner){
-        this.addMouseListener(listner); 
-        this.addMouseMotionListener(listner);
+    private static Drawboard drawboard;
+    private static List<Component> componentList;
+
+    @Override
+    public Component add(Component comp) {
+        Component c = super.add(comp);
+        if (componentList == null){
+            componentList = new ArrayList<Component>();
+        }
+        componentList.add(comp);
+        return c;
     }
+
+    public static Drawboard getInstance(){
+        if (drawboard == null){
+            drawboard=new Drawboard();
+        }
+        return drawboard;
+    }
+    
     
     /**
      * 这个方法重写了JFrame的画图方法,将会把所有绘制的图形全部绘制一次。
@@ -38,5 +59,8 @@ public class Drawboard extends JPanel{
             shape.draw((Graphics2D)bfpen);
         }
         g.drawImage(bfimg, 0, 0, this);
+        for (Component c: componentList){
+            c.repaint();
+        }
     }
 }
