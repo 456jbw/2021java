1d08ccf6edbc404d9c6fd97eefd2178aac3e321e
Costwen <47916831+Costwen@users.noreply.github.com>
2021-06-10 15:49:34 +0800

    ✨ feat: 实现了颜色选择器

---
+++ src/listener/MyColorListener.java
@@ -0,0 +1,23 @@
+package listener;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import views.Drawboard;
+
+
+public class MyColorListener implements ActionListener {
+    private static MyColorListener myColorListener;
+    public static MyColorListener getInstance(){
+        if (myColorListener == null){
+            myColorListener = new MyColorListener();
+        }
+        return myColorListener;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton instance = (JButton) e.getSource();
+        Color color = instance.getBackground();
+        System.out.println(color);
+        MyDrawListener.getInstance().setColor(color);
+    }
+}
---
+++ src/views/Colorlist.java
@@ -0,0 +1,43 @@
+package views;
+import javax.swing.*;
+
+import listener.MyColorListener;
+import listener.MyDrawListener;
+
+import java.awt.*;
+
+// 颜色选择列表
+public class Colorlist extends JPanel {
+    static final long serialVersionUID = 1471001741;
+
+    public Colorlist() {
+        // 得到监听器实例
+        MyColorListener listener = MyColorListener.getInstance();
+        // 为列表使用二行四列的栅格布局
+        this.setLayout(new GridLayout(2, 4, 2, 2));
+        // 通过颜色数组快速构建前七个按钮
+        Color[] colorArray = { Color.BLACK, Color.BLUE, Color.YELLOW, Color.GREEN, Color.PINK, Color.RED, Color.CYAN };
+        for (Color item : colorArray) {
+            JButton tmp = new JButton();
+            tmp.setBackground(item);
+            // 为该按钮添加点击监听，详见EventListener->actionPerformed
+            tmp.addActionListener(listener);
+            this.add(tmp);
+        }
+        // 最后一个按钮是自定义颜色
+        JButton customColor = new JButton();
+        customColor.setBackground(Color.WHITE);
+        // 为该按钮加入与其它按钮相同的监听
+        customColor.addActionListener(listener);
+        // 点击弹出颜色对话框，将选中颜色设置为背景色
+        // 注意：经过测试，多个ActionListener的情况下会优先执行后添加的
+        customColor.addActionListener(e -> {
+            Color selectedColor = JColorChooser.showDialog(null, "自定义颜色", Color.BLACK);
+            if (selectedColor == null) {
+                selectedColor = Color.WHITE;
+            }
+            customColor.setBackground(selectedColor);
+        });
+        this.add(customColor);
+    }
+}
